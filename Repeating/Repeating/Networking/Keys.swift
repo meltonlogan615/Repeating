65d3a7da3a0c3eac8ddc7fdc26a2956957a6c85b
Logan <meltonlogan@me.com>
2023-02-27 19:37:31 -0600

    Added Google Taxonomy File and APIKey for Barcode Lookup

---
+++ Repeating/Repeating/Networking/Keys.swift
@@ -0,0 +1,13 @@
+//
+//  Keys.swift
+//  Repeating
+//
+//  Created by Logan Melton on 2/27/23.
+//
+
+import Foundation
+
+struct Keys {
+  // Edamam PRoduct Lookup Key
+  static let barcodeKey = "f713df30be4354d919578f14c4eb9547"
+}
